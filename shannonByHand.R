c30c69578b87e589f21876017c013522563ef08e
hamsternik <hamsternik9@gmail.com>
2015-06-28 02:24:57 +0300

    small 'beauty-code' changes

--- shannonByHand.R
+++ shannonByHand.R
@@ -1,14 +1,18 @@
 # Shannon entropy method "by hand"
+require(fgui)
 shannonByHand <- function(inpVec) {
   #y <- readline("Please, enter number series from keyboard: ")
   newInpVec <- c()
+  
   for (i in 1:length(inpVec)) {
     Z <- (inpVec[i] - min(inpVec)) / (max(inpVec) - min(inpVec))
     newInpVec <- append(newInpVec, Z)
   }
+  
   freqVec <- table(newInpVec) / length(newInpVec)
   formedVec <- as.data.frame(freqVec)[,2]
   H = -sum(formedVec * log2(formedVec))
+  
   print(H)
 }
 
